9659b974be76ef30f425194ff1dc12b0232dd25e
mojaloss30-collab <mojaloss30@gmail.com>
2025-09-09 15:42:14 +0200

    Update functions_start.py

--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -1,12 +1,13 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for working with functions
 
-
 # define a basic function
-print("hello world!")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
+def hello_func():
+  print("hello world!")
+  name = input("What is your name? ")
+  print("Nice to meet you,", name)
 
+hello_func()
 # function that takes parameters
 
 
